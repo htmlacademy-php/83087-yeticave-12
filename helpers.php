fcb266abe04ab576ad2a17943c06d45a21e4f239
Павел <papan41k@gmail.com>
2021-07-22 20:01:27 +0300

    Исправление

--- helpers.php
+++ helpers.php
@@ -1,5 +1,21 @@
 <?php
 
+if (!defined('LOTS_PER_PAGE')) {
+    define('LOTS_PER_PAGE', 9);
+}
+if (!defined('NAME_LENGTH_LIMIT')) {
+    define('NAME_LENGTH_LIMIT', 128);
+}
+if (!defined('TEXT_LENGTH_LIMIT')) {
+    define('TEXT_LENGTH_LIMIT', 65535);
+}
+if (!defined('LOT_PRICE_LIMIT')) {
+    define('LOT_PRICE_LIMIT', 65);
+}
+if (!defined('LOT_RATE_LIMIT')) {
+    define('LOT_RATE_LIMIT', 2147483647);
+}
+
 /**
  * Создает подготовленное выражение на основе готового SQL запроса и переданных данных
  *
